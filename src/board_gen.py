30dc318d1d0cf0d7d3b1f4904feb94a14017cfa9
Ryan Ozawa <ryan.ozawa21@gmail.com>
2021-06-17 19:13:41 -0700

    add docstrings, plan out generate_board

--- src/board_gen.py
+++ src/board_gen.py
@@ -1,6 +1,7 @@
 import random
-from sudoku import *
+from typing import List
 import pickle
+from sudoku import *
 
 
 class Generator:
@@ -21,13 +22,13 @@ class Generator:
         [0, 0, 0, "c", "a", "f", 0, "e", "i"],
     ]
 
-    def __init__(self, filename) -> None:
+    def __init__(self, filename: str = None) -> None:
         """
         Parameters
         ----------
         filename : str
             location of the pickled boards file. 
-            If None or empty string, defaults to Boards/board.pkl
+            If None or empty string, defaults to Boards/board.pkl and will create the default template
         """
         if not filename:
             self.board_file = "Boards/board.pkl"
@@ -36,7 +37,7 @@ class Generator:
             self.board_file = filename
         self.boards = []
 
-    def create_board(self):
+    def create_board(self) -> List[list]:
         """Randomly choose a template stored in self.boards and return a playable board
 
         Parameters 
@@ -47,6 +48,7 @@ class Generator:
         list[list[int]]
             A board of ints
         """
+        board = random.choice(self.boards)
         val_dict = {
             "a": 1,
             "b": 1,
@@ -62,11 +64,11 @@ class Generator:
         random.shuffle(val_keys)
         for i, x in enumerate(val_keys, start=1):
             val_dict[x] = i
-        for x in range(9):
-            for y in range(9):
-                if isinstance(data[x][y], str):
-                    data[x][y] = val_dict[data[x][y]]
-        return data
+        for i in range(9):  # board is 9x9
+            for j in range(9):
+                if isinstance(board[i][j], str):  # only non blanks are string
+                    board[i][j] = val_dict[board[i][j]]
+        return board
 
     def load_board_file(self) -> None:
         """Loads the board stored in a pickled file to the object
@@ -83,27 +85,47 @@ class Generator:
         """
         pickle.dump(self.boards, open(self.board_file, "wb"))
 
+    @staticmethod
     def format_board(data):
+        """Takes in a list of lists of strings representing a board or boards
+        Turns string zeros into ints so board creation works
+
+        Parameters
+        ----------
+        data : List[str]
+            List of strings representing board
+
+        Returns
+        -------
+        List[list[Union(str, int)]]
+        """
+        data = [line.split() for line in data]
         for line in range(len(data)):
-            data[line] = data[line].split()
             for item in range(len(data[line])):
                 if data[line][item] == "0":  # get rid of string zeros
                     data[line][item] = 0
         return data
 
-    def get_mul_boards(filename):
-        data = ""
+    def get_mul_boards(self, filename: str) -> None:
+        """Read in boards from a given plaintext file to store in object
+
+        Board format in plain text should be - 9 rows of 9 elements separated by a space
+
+        Parameters
+        ----------
+        filename : str
+            str representing path to plaintext file
+        """
         with open(filename, "r") as file:
             data = file.readlines()
-        board = Generator.format_board(data)
-        for i in range(len(board) // 9):
-            startLine = 9 * i
-            temp = []
-            for j in range(9):
-                temp.append(board[i + j])
-            Generator.boards.append(temp)
+        all_boards = self.format_board(data)
+        for i in range(0, len(all_boards), 9):
+            self.boards.append(all_boards[i : i + 9])
 
-    def gen_board():
+    def generate_board_template(self, board: List[List[str]]) -> None:
+        """Make a new board template
+        TODO: take in a solved board, randomly choose places to blank. Test for solvability
+        """
         pass
 
 
