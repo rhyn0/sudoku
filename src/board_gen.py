311908b7b646ee426b5b85906183bb3b105786e3
Ryan <ryan.ozawa21@gmail.com>
2021-06-19 12:50:08 -0700

    begin board gen, problem with box2 gen

--- src/board_gen.py
+++ src/board_gen.py
@@ -31,7 +31,7 @@ class Generator:
             If None or empty string, defaults to Boards/board.pkl and will create the default template
         """
         if not filename:
-            self.board_file = "Boards/board.pkl"
+            self.board_file = "src/Boards/board.pkl"
             pickle.dump(self.PREMADE_BOARD, open(self.board_file, "wb"))
         else:
             self.board_file = filename
@@ -86,6 +86,22 @@ class Generator:
         pickle.dump(self.boards, open(self.board_file, "wb"))
 
     @staticmethod
+    def pretty_print(board) -> None:
+        """Output a pretty board with surrounding horizontal lines. Print to stdout
+
+        Parameters
+        ----------
+        board : List[List[int]]
+            board in list of lists format
+        """
+        print("-" * 79)
+        for i in range(9):
+            for j in range(9):
+                print(f" {board[i][j]}", end="")
+            print()
+        print("-" * 79)
+
+    @staticmethod
     def format_board(data):
         """Takes in a list of lists of strings representing a board or boards
         Turns string zeros into ints so board creation works
@@ -123,11 +139,74 @@ class Generator:
             self.boards.append(all_boards[i : i + 9])
 
     def generate_board_template(self, board: List[List[str]]) -> None:
-        """Make a new board template
-        TODO: take in a solved board, randomly choose places to blank. Test for solvability
+        """Make a new board template from a fully solved list
+        TODO: use Daniel Beer method
         """
         pass
 
+    def generate_new_board(self):
+        ORDER = 3
+        FULL_SET = set([i for i in range(1, 10)])
+        # board demarks the actual board, choices are all possibilities for a cell
+        board = [[0 for _ in range(9)] for _ in range(9)]
+        choices = [[set() for _ in range(9)] for _ in range(9)]
 
-# if __name__ == "__main__":
-#     print_board(Generator.create_board(Generator.get_board_file("boards.txt")))
+        def choose_box1(grid):
+            # modify in place, the first box of the board
+            box1 = FULL_SET.copy()
+            for i in range(ORDER):
+                for j in range(ORDER):
+                    v = random.choice(list(box1))
+                    box1.remove(v)
+                    grid[i][j] = v
+
+        def choose_box2(grid):
+            # modify in place, the second box based on first box
+            rows = [set() for _ in range(ORDER)]
+            choose = [set() for _ in range(ORDER)]
+            for i in range(ORDER):
+                for el in grid[i][:3]:
+                    rows[i].add(el)
+            free_set = rows[1].union(rows[2])
+            for i in range(ORDER):
+                choose[0].add(random.choice(list(free_set)))
+            middle_set = rows[0].union(rows[1]).difference(choose[0])
+            last_set = rows[0].union(rows[2]).difference(choose[0])
+            while len(last_set) > 3:
+                v = random.choice(list(middle_set))
+                choose[1].add(v)
+                middle_set.remove(v)
+                if v in last_set:
+                    last_set.remove(v)
+            choose[1] = choose[1].union(middle_set).difference(last_set)
+            choose[2] = last_set.copy()
+
+            for i in range(ORDER):
+                l_choose = list(choose[i])
+                random.shuffle(l_choose)
+                grid[i][ORDER : 2 * ORDER] = l_choose
+
+        def choose_box3(grid):
+            # modify in place, choose last possible values
+            pass
+
+        def choose_col(grid):
+            # modify in place, choose left column
+            pass
+
+        def init_choices(grid, freedom):
+            # decide possible values for blank/0 cells
+            pass
+
+        def choose_rest(grid, freedom):
+            # recursively solve the board, allows backtracking
+            pass
+
+        choose_box1(board)
+        choose_box2(board)
+        Generator.pretty_print(board)
+
+
+if __name__ == "__main__":
+    gen = Generator()
+    gen.generate_new_board()
