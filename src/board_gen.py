66adabcf7c9e16d4de48996b5b200b8810bf9229
Ryan Ozawa <ryan.ozawa21@gmail.com>
2021-06-23 13:32:59 -0700

    gen of new template done, plan finding difficulty

--- src/board_gen.py
+++ src/board_gen.py
@@ -2,6 +2,7 @@ import random
 from typing import List, Tuple, Union
 import pickle
 from copy import deepcopy
+import os.path
 
 
 class Generator:
@@ -11,15 +12,17 @@ class Generator:
     """
 
     PREMADE_BOARD = [
-        ["e", "f", 0, "h", "d", "g", 0, 0, 0],
-        ["c", 0, "i", 0, 0, 0, "f", 0, 0],
-        [0, 0, "h", 0, 0, 0, 0, 0, 0],
-        [0, "a", 0, 0, "h", 0, 0, "d", 0],
-        ["g", "i", 0, "f", 0, "b", 0, "a", "h"],
-        [0, "e", 0, 0, "c", 0, 0, "i", 0],
-        [0, 0, 0, 0, 0, 0, "b", 0, 0],
-        [0, 0, "f", 0, 0, 0, "h", 0, "g"],
-        [0, 0, 0, "c", "a", "f", 0, "e", "i"],
+        [
+            ["e", "f", 0, "h", "d", "g", 0, 0, 0],
+            ["c", 0, "i", 0, 0, 0, "f", 0, 0],
+            [0, 0, "h", 0, 0, 0, 0, 0, 0],
+            [0, "a", 0, 0, "h", 0, 0, "d", 0],
+            ["g", "i", 0, "f", 0, "b", 0, "a", "h"],
+            [0, "e", 0, 0, "c", 0, 0, "i", 0],
+            [0, 0, 0, 0, 0, 0, "b", 0, 0],
+            [0, 0, "f", 0, 0, 0, "h", 0, "g"],
+            [0, 0, 0, "c", "a", "f", 0, "e", "i"],
+        ]
     ]
     ORDER = 3
     FULL_SET = set([i for i in range(1, 10)])
@@ -34,10 +37,28 @@ class Generator:
         """
         if not filename:
             self.board_file = "src/Boards/board.pkl"
-            pickle.dump(self.PREMADE_BOARD, open(self.board_file, "wb"))
+            if not os.path.isfile(self.board_file):
+                with open(self.board_file, "wb") as fp:
+                    pickle.dump(self.PREMADE_BOARD, fp)
         else:
             self.board_file = filename
         self.boards = []
+
+    def templatize_board(self, board: List[List[int]]) -> None:
+        """Takes a board and converts clues to string placeholders. Appends result to self.boards
+
+        Parameters
+        ----------
+        board : List[List[int]]
+            solvable board with clues
+        """
+        temp = []
+        holders = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        random.shuffle(holders)
+        d = {i: k for i, k in enumerate(holders, start=1)}
+        for row in board:
+            temp.append([d[k] if k != 0 else 0 for k in row])
+        self.boards.append(temp)
 
     def create_board(self) -> List[list]:
         """Randomly choose a template stored in self.boards and return a playable board
@@ -51,21 +72,9 @@ class Generator:
             A board of ints
         """
         board = random.choice(self.boards)
-        val_dict = {
-            "a": 1,
-            "b": 1,
-            "c": 1,
-            "d": 1,
-            "e": 1,
-            "f": 1,
-            "g": 1,
-            "h": 1,
-            "i": 1,
-        }
-        val_keys = list(val_dict.keys())
-        random.shuffle(val_keys)
-        for i, x in enumerate(val_keys, start=1):
-            val_dict[x] = i
+        holders = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        random.shuffle(holders)
+        val_dict = {x: i for i, x in enumerate(holders, start=1)}
         for i in range(9):  # board is 9x9
             for j in range(9):
                 if isinstance(board[i][j], str):  # only non blanks are string
@@ -76,7 +85,11 @@ class Generator:
         """Loads the board stored in a pickled file to the object
         """
         try:
-            self.boards = pickle.load(open(self.board_file, "rb"))
+            with open(self.board_file, "rb") as fp:
+                temp_boards = pickle.load(fp)
+            for b in temp_boards:
+                if b not in self.boards:
+                    self.boards.append(b)
         except FileNotFoundError:
             print(
                 f"Error: file {self.board_file} does not exist. No boards were loaded"
@@ -85,7 +98,8 @@ class Generator:
     def output_boards(self) -> None:
         """Writes the board templates to stored board_file, in a pickled format
         """
-        pickle.dump(self.boards, open(self.board_file, "wb"))
+        with open(self.board_file, "wb") as fp:
+            pickle.dump(self.boards, fp)
 
     @staticmethod
     def check_box(table: List[List[int]], i: int, j: int, cell_val: int) -> int:
@@ -212,88 +226,77 @@ class Generator:
             self.boards.append(all_boards[i : i + 9])
 
     @classmethod
-    def get_board_diffic(cls, board):
-        pass
+    def get_board_diffic(cls, board, freedom=None):
+        if freedom is None:
+            freedom = cls.init_choices(board)
+        diff = 0
+        for row in freedom:
+            for st in row:
+                diff += len(st) ** 2
+        return diff
 
-    def generate_board_template(self, board: List[List[int]], max_iter: int) -> None:
+    def generate_board_template(self, solution: List[List[int]], max_iter: int):
         """Make a new board template from a fully solved board
-        # TODO: use Daniel Beer method harden
+        
+        Will append to self.boards. Either removes clues on the board or adds a pair back in. 
+        Increasing max_iter will make a more difficult puzzle.
         """
-        best = 0  # keep track of difficulty
+        board = deepcopy(solution)
         for _ in range(max_iter):
             problem = deepcopy(board)
             for _ in range(20):
-                c, r = random.randint(0, 8), random.randint(0, 8)
+                s = random.randint(0, 80)
+                r, c = s // 9, s % 9
+                # flip a coin on removing a pair of values, or adding them in
                 if random.random() < 0.5:
                     problem[r][c] = 0
                     problem[8 - r][8 - c] = 0
                 else:
-                    pass
-
-    def choose_box1(self, grid):
-        # modify in place, the first box of the board
-        box1 = self.FULL_SET.copy()
-        for i in range(self.ORDER):
-            for j in range(self.ORDER):
-                v = random.choice(list(box1))
-                box1.remove(v)
-                grid[i][j] = v
-
-    def choose_box2(self, grid):
-        # modify in place, the second box based on first box
-        rows = [set() for _ in range(self.ORDER)]
-        choose = [set() for _ in range(self.ORDER)]
-        for i in range(self.ORDER):
-            for el in grid[i][:3]:
-                rows[i].add(el)
-        free_set = rows[1].union(rows[2])
-        for i in range(self.ORDER):
-            v = random.choice(list(free_set))
-            choose[0].add(v)
-            free_set.remove(v)
-        middle_set = rows[0].union(rows[2]).difference(choose[0])
-        last_set = rows[0].union(rows[1]).difference(choose[0])
-        while len(last_set) > 3:
-            v = random.choice(list(middle_set))
-            choose[1].add(v)
-            middle_set.remove(v)
-            last_set.discard(v)  # discard throws no error if v doesn't exist
-        choose[1] = choose[1].union(middle_set.difference(last_set))
-        choose[2] = last_set.copy()
-        for i in range(self.ORDER):
-            l_choose = list(choose[i])
-            random.shuffle(l_choose)
-            grid[i][self.ORDER : 2 * self.ORDER] = l_choose
-
-    def choose_box3(self, grid):
-        # modify in place, choose last possible values
-        for i in range(self.ORDER):
-            # for each actual row, compute remaining choices and then fill
-            used = set()
-            for j in range(self.ORDER * 2):
-                used.add(grid[i][j])
-            free = list(self.FULL_SET.difference(used))
-            random.shuffle(free)
-            grid[i][self.ORDER * 2 : self.ORDER * self.ORDER] = free
-
-    def choose_col(self, grid):
-        # modify in place, choose left column
-        used = set()
-        for i in range(self.ORDER):
-            used.add(grid[i][0])
-        free = self.FULL_SET.difference(used)
-        for i in range(self.ORDER * 2):
-            v = random.choice(list(free))
-            free.remove(v)
-            grid[self.ORDER + i][0] = v
+                    problem[r][c] = solution[r][c]
+                    problem[8 - r][8 - c] = solution[8 - r][8 - c]
+                choices = self.init_choices(problem)
+                if self.is_legal_board(problem, choices):
+                    test = deepcopy(problem)
+                    if self.choose_rest(test, choices) == 0:
+                        board = problem
+        self.templatize_board(board)
 
     @classmethod
-    def init_choices(cls, grid, freedom: List[List[set]]):
+    def init_choices(cls, grid) -> List[List[set]]:
         # decide possible values for blank/0 cells
+        freedom = [[cls.FULL_SET.copy() for _ in range(9)] for _ in range(9)]
         for y in range(len(grid)):
             for x in range(len(grid[y])):
                 if grid[y][x] != 0:
                     cls.remove_freedom(freedom, x, y, grid[y][x])
+        return freedom
+
+    @classmethod
+    def remove_freedom(cls, freedom: List[List[set]], x: int, y: int, val: int):
+        saved = freedom[y][x].copy()
+        # remove from column
+        for i in range(cls.ORDER ** 2):
+            freedom[i][x].discard(val)
+
+        # remove from row
+        for i in range(cls.ORDER ** 2):
+            freedom[y][i].discard(val)
+
+        # remove from box
+        start_row = y - y % cls.ORDER
+        start_col = x - x % cls.ORDER
+        for i in range(start_row, start_row + cls.ORDER):
+            for j in range(start_col, start_col + cls.ORDER):
+                freedom[i][j].discard(val)
+        freedom[y][x] = saved
+
+    @classmethod
+    def is_legal_board(cls, grid, freedom: List[List[set]]) -> bool:
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                if grid[y][x] != 0 and grid[y][x] not in freedom[y][x]:
+                    return False
+        return True
 
     @classmethod
     def choose_rest(cls, grid, freedom: List[List[set]]):
@@ -339,28 +342,84 @@ class Generator:
         copyright notice and this permission notice appear in all copies.
         """
 
+        def choose_box1(grid):
+            # modify in place, the first box of the board
+            box1 = self.FULL_SET.copy()
+            for i in range(self.ORDER):
+                for j in range(self.ORDER):
+                    v = random.choice(list(box1))
+                    box1.remove(v)
+                    grid[i][j] = v
+
+        def choose_box2(grid):
+            # modify in place, the second box based on first box
+            rows = [set() for _ in range(self.ORDER)]
+            choose = [set() for _ in range(self.ORDER)]
+            for i in range(self.ORDER):
+                for el in grid[i][:3]:
+                    rows[i].add(el)
+            free_set = rows[1].union(rows[2])
+            for i in range(self.ORDER):
+                v = random.choice(list(free_set))
+                choose[0].add(v)
+                free_set.remove(v)
+            middle_set = rows[0].union(rows[2]).difference(choose[0])
+            last_set = rows[0].union(rows[1]).difference(choose[0])
+            while len(last_set) > 3:
+                v = random.choice(list(middle_set))
+                choose[1].add(v)
+                middle_set.remove(v)
+                last_set.discard(v)  # discard throws no error if v doesn't exist
+            choose[1] = choose[1].union(middle_set.difference(last_set))
+            choose[2] = last_set.copy()
+            for i in range(self.ORDER):
+                l_choose = list(choose[i])
+                random.shuffle(l_choose)
+                grid[i][self.ORDER : 2 * self.ORDER] = l_choose
+
+        def choose_box3(grid):
+            # modify in place, choose last possible values
+            for i in range(self.ORDER):
+                # for each actual row, compute remaining choices and then fill
+                used = set()
+                for j in range(self.ORDER * 2):
+                    used.add(grid[i][j])
+                free = list(self.FULL_SET.difference(used))
+                random.shuffle(free)
+                grid[i][self.ORDER * 2 : self.ORDER * self.ORDER] = free
+
+        def choose_col(grid):
+            # modify in place, choose left column
+            used = set()
+            for i in range(self.ORDER):
+                used.add(grid[i][0])
+            free = self.FULL_SET.difference(used)
+            for i in range(self.ORDER * 2):
+                v = random.choice(list(free))
+                free.remove(v)
+                grid[self.ORDER + i][0] = v
+
         # board demarks the actual board, choices are all possibilities for a cell
         board = [[0 for _ in range(9)] for _ in range(9)]
-        choices = [[self.FULL_SET.copy() for _ in range(9)] for _ in range(9)]
-        self.choose_box1(board)
-        self.choose_box2(board)
-        self.choose_box3(board)
-        self.choose_col(board)
-        self.init_choices(board, choices)
+        choose_box1(board)
+        choose_box2(board)
+        choose_box3(board)
+        choose_col(board)
+        choices = self.init_choices(board)
         self.choose_rest(board, choices)
+        return board
 
-    @classmethod
-    def generate_board(cls) -> List[List[int]]:
+    def generate_board(self) -> List[List[int]]:
         board = [[0 for _ in range(9)] for _ in range(9)]
-        choices = [[cls.FULL_SET.copy() for _ in range(9)] for _ in range(9)]
+        choices = [[self.FULL_SET.copy() for _ in range(9)] for _ in range(9)]
 
         def choose_box(x, y, grid):
             # set to top left of the respective box
-            x -= x % cls.ORDER
-            y -= y % cls.ORDER
-            curr = cls.FULL_SET.copy()
-            for i in range(cls.ORDER):
-                for j in range(cls.ORDER):
+            x -= x % self.ORDER
+            y -= y % self.ORDER
+            curr = self.FULL_SET.copy()
+            for i in range(self.ORDER):
+                for j in range(self.ORDER):
                     v = random.choice(list(curr))
                     curr.remove(v)
                     grid[y + i][x + j] = v
@@ -368,37 +427,17 @@ class Generator:
         choose_box(0, 0, board)
         choose_box(3, 3, board)
         choose_box(6, 6, board)
-        cls.init_choices(board, choices)
-        cls.choose_rest(board, choices)
+        choices = self.init_choices(board)
+        self.choose_rest(board, choices)
         return board
-
-    @classmethod
-    def remove_freedom(cls, freedom: List[List[set]], x: int, y: int, val: int):
-
-        # remove from column
-        for i in range(cls.ORDER ** 2):
-            freedom[i][x].discard(val)
-
-        # remove from row
-        for i in range(cls.ORDER ** 2):
-            freedom[y][i].discard(val)
-
-        # remove from box
-        start_row = y - y % cls.ORDER
-        start_col = x - x % cls.ORDER
-        for i in range(start_row, start_row + cls.ORDER):
-            for j in range(start_col, start_col + cls.ORDER):
-                freedom[i][j].discard(val)
 
 
 if __name__ == "__main__":
     gen = Generator()
-    b = gen.generate_board()
-    gen.pretty_print(b)
+    gen.load_board_file()
+    print(len(gen.boards))
+    b = gen.generate_beer_board()
+    gen.generate_board_template(b, 5)
+    print(len(gen.boards))
+    gen.pretty_print(gen.boards[-1])
 
-
-"""
-To calculate the difficulty of a board, we add the square of the freedom of each
-cell as it gets filled in and we dive deeper recursively. Since this uses local variable
-its value will be passed along instead of its reference and will only return when fully solved.
-"""
